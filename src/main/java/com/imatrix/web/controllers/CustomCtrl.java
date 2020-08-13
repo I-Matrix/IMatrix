de2a92476e6b72ba87c3ecdaafb58e8f9bf3fda4
amanuel2 <abogale2@gmail.com>
2020-08-12 23:06:19 -0400

    Pretty much finished with changes, might look into more in the future

--- src/main/java/com/imatrix/web/controllers/CustomCtrl.java
+++ src/main/java/com/imatrix/web/controllers/CustomCtrl.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class CustomCtrl {
 	
 	@Autowired
 	private AmazonClient client;
+	
+	@GetMapping("/custom")
+	public ModelAndView customGet() {
+		return new ModelAndView("customGet");
+	}
 	
 	@PostMapping("/custom")
 	public ModelAndView custom(@RequestParam("file") MultipartFile file, 
