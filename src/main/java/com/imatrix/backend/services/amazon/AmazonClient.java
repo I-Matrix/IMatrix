3990a9f35ccedb780736ae009947d9a94ee06104
amanuel2 <abogale2@gmail.com>
2020-08-19 02:59:24 -0400

    Amazon documentation. Documentation 1/8 done

--- src/main/java/com/imatrix/backend/services/amazon/AmazonClient.java
+++ src/main/java/com/imatrix/backend/services/amazon/AmazonClient.java
@@ -3,16 +3,131 @@ package com.imatrix.backend.services.amazon;
 import java.io.File;
 import java.io.IOException;
 
+import com.amazonaws.AmazonServiceException;
+
+/**
+* <h1>Amazon Client Interface</h1>
+* <p>
+* 	Implementation of this interface is posted
+* 	on the same package renamed 'impl'
+* </p>
+* 
+* <p>
+* 	This interface allows users to create buckets,
+* 	print the list of buckets in that s3 database
+* 	upload as well as delete files to a particular bucket
+* 	and finally download files from the bucket as 
+* 	a String.
+* </p>
+* 
+* <p>
+* 	<b>This isn't the full width</b> of all functions
+*   the Amazon interface allows us to utilize but only
+*   necessary.
+* </p>
+* 
+* @author  Amanuel Bogale
+* @version 0.1
+* @since   2020-08-20
+* @see     AmazonClientImpl
+*/
 public interface AmazonClient {
 	
-	public void createBucket(String bucketName);
+  /**<p>
+   * This method creates a bucket with a specific name
+   * user specifies. If a bucket with same name is
+   * already created, this will print an warning to the
+   * standard error, {@code System.err}
+   * </p>
+   * 
+   * @param  bucketName name of the bucket that will
+   * 		 be added as specified up top. 
+   * @return returns success if bucket doesnt already exist
+   * 		 {@code Constants.SUCCESS} or a failure
+   * 		 if otherwise {$code Constants.FAILURE}
+   */
+	public int createBucket(String bucketName);
 	
+  /**<p>
+   * This method prints all buckets that are
+   * available in the s3 database provided in
+   * {@value application.yml} with the secret keys
+   * {@code Impl.secretKey} and {@code Impl.accessKey}
+   * </p>
+   * 
+   * @param  {@code (void)}
+   * @return {@code (void)}
+   */
 	public void printBuckets();
 	
-	public void uploadFileToBucket(String bucket, String fileName, File file);
+  /**<p>
+   * This method's main purpose is to upload a file
+   * located under 
+   * 	<pre>'{@code System.get(user.home)}/.uploadIMatrix'</pre>
+   * to the s3 bucket specified by the user. 
+   * </p>
+   * 
+   * @param  bucketName name of the bucket that will
+   * 		 have the file added
+   * @param  <b>filename only</b> of the file 
+   * 		 being uploaded
+   * @param  Specific file under above mentioned
+   * 		 path to be uploaded
+   * @return returns success if file uploaded successfully
+   * 		 to bucket {@code Constants.SUCCESS} or a failure
+   * 		 otherwise {@code Constants.FAILURE}
+   * 
+   * @throws AmazonServiceException
+   *         If any errors occurred in Amazon S3 while processing the
+   *         request.
+   */
+	public int uploadFileToBucket(String bucket, String fileName, File file);
 	
-	public void deleteFileToBucket(String fileName, String bucket);
 	
-	public String downloadFileFromBucket(String bucket, String fileName)throws IOException ;
+  /**<p>
+   * This methods main purpose is to delete
+   * a specific file specified by the user
+   * from the bucket. File located in :
+   * 	<pre>'{@code System.get(user.home)}/.uploadIMatrix'</pre>
+   * </p>
+   * 
+   * @param  bucketName name of the bucket that will
+   * 		 have the file added
+   * @param  <b>filename only</b> of the file 
+   * 		 being uploaded
+   * @param  Specific file under above mentioned
+   * 		 path to be uploaded
+   * @return {@code (void)}
+   * 
+   * @throws AmazonServiceException
+   *         If any errors occurred in Amazon S3 while processing the
+   *         request.
+   */
+	
+	public void deleteFileToBucket(String bucket, String fileName);
+	
+	
+  /**<p>
+   * This methods main purpose is to download
+   * a file from the bucket both specified by
+   * the user as @param's
+   * </p>
+   * 
+   * @param  bucketName name of the bucket that will
+   * 		 have the file to download from
+   * @param  <b>filename only</b> of the file 
+   * 		 being downloaded
+   * @return returns the string of the path where
+   * 		 the file from the bucket has been
+   * 		 downloaded to in the main server.
+   * 		 Its main location is located:
+   * 
+   * 				<pre>'{@code System.get(user.home)}/.uploadIMatrix'</pre>
+   * 
+   * @throws IOException
+   *         If any errors occurred of searching for the file
+   *         specified by the fileName as a @param.
+   */
+	public String downloadFileFromBucket(String bucket, String fileName) throws IOException;
 	
 }
--- src/main/java/com/imatrix/backend/services/amazon/AmazonClientImpl.java
+++ src/main/java/com/imatrix/backend/services/amazon/AmazonClientImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.mediastoredata.model.PutObjectResult;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
@@ -22,12 +23,42 @@ import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.imatrix.backend.util.resources.Constants;
 
+/**
+* <h1>Amazon Client Implementation</h1>
+* <p>
+* 	Implementation of this interface
+* </p>
+* 
+* <p>
+* 	This is the implementation of the AmazonClient interface.
+*   @see {@interface AmazonClient} for more details
+* </p>
+* 
+* <p>
+* 	<b>This isn't the full implementation</b> of all functions
+*   the Amazon interface allows us to utilize but only
+*   necessary.
+* </p>
+* 
+* @author  Amanuel Bogale
+* @version 0.1
+* @since   2020-08-20
+* @see     AmazonClient
+*/
 @Service
 public class AmazonClientImpl implements AmazonClient {
 
+	/**
+	 * Main S3 Object
+	 */
 	private AmazonS3 s3client;
 	
+	/**
+	 * All values driven from the property file
+	 * <pre> 'res/application.yml' </p>
+	 */
 	@Value("${amazonProperties.endpointUrl}")
     private String endpointUrl;
     
@@ -45,37 +76,66 @@ public class AmazonClientImpl implements AmazonClient {
 		AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
 		this.s3client = new AmazonS3Client(credentials);
     }
-    
-    
-    public void createBucket(String bucketName) {
+	    
+  /**
+	* Creates a bucket from the name
+	* detail explanation in interface
+	* @see AmazonClient.java
+	*/
+    public int createBucket(String bucketName) {
     	if(s3client.doesBucketExist(bucketName)) {
     		System.err.println("BUCKET ALREADY EXISTS");
-    		return;
+    		return Constants.FAILURE;
     	}
     	s3client.createBucket(new CreateBucketRequest(bucketName));
+    	return Constants.SUCCESS;
     }
     
+   /**
+    * Prints all the buckets in the
+    * s3 database
+    * @see AmazonClient.java for more info
+    */
     public void printBuckets() {
     	List<Bucket> buckets = s3client.listBuckets();
     	for(Bucket bucket: buckets) {
     		System.out.println(bucket.getName());
     	}
     }
-    public void uploadFileToBucket(String bucket, String fileName, File file) {
+    
+   /**
+    * Uploads specified file to the
+    * specified bucket
+    * @see AmazonClient.java for more info
+    */
+    public int uploadFileToBucket(String bucket, String fileName, File file) {
+    	com.amazonaws.services.s3.model.PutObjectResult res = 
     	this.s3client.putObject(new PutObjectRequest(
     							 bucket, 
     							 fileName,
     							 file
     						   ));
+    	if(res == null) return Constants.FAILURE;
+    	return Constants.SUCCESS;
     }
     
-    public void deleteFileToBucket(String fileName, String bucket) {
+   /**
+    * Deletes specified file to the
+    * specified bucket
+    * @see AmazonClient.java for more info
+    */
+    public void deleteFileToBucket(String bucket, String fileName) {
     	this.s3client.deleteObject(new DeleteObjectRequest(
     								bucket,
     								fileName
     							  ));
     }
     
+   /**
+    * Downloads specified file to the
+    * specified bucket
+    * @see AmazonClient.java for more info
+    */
     public String downloadFileFromBucket(String bucket, String fileName) throws IOException {    	
     	GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucket, fileName);
     	URL url = s3client.generatePresignedUrl(request);
